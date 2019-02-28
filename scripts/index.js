faceb40cfb4d710cb29e104185696f9b03ae7722
iTrauco <Chris@Trau.co>
2019-02-28 11:35:35 -0500

    adds click handler to ALL target elements

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,16 @@ const targetElements = document.querySelectorAll("[data-target]"); // const is a
 
 // when we click any picture-frame class we seen an enlarged version of image inside of .picture-frame
 //
-targetElements[0].addEventListener("click", respondToClick); //
 
 function respondToClick() {
-    console.log('test');
-    console.log('how are you?');
-    console.log('I am fine.');
+    console.log('test click line 1');
+    console.log('test click line 2');
+    console.log('test clicl line 3');
 }
+
+function attachClickHandler(oneElement) {
+    oneElement.addEventListener("click", respondToClick);
+}
+// this is how to handle one element
+//targetElements[0].addEventListener("click", respondToClick);
+targetElements.forEach(attachClickHandler);
